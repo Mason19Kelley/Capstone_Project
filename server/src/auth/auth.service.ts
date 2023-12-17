f4b6927f99e39fc2f1aef34fd95480be5095cd60
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2023-12-16 19:57:34 -0600

    added hashing to user password

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,8 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findUser(username);
-    if (user && user.password === pass) {
+    const hashedPass = await bcrypt.hash(pass);
+    if (user && user.password === hashedPass) {
       const { password, ...result } = user;
       return result;
     }
@@ -24,5 +26,7 @@ export class AuthService {
       access_token: this.jwtService.sign(payload),
     };
   }
+
+  
 
 }
