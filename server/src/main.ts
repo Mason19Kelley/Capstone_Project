f566eec9fbaf35cdab75c22bfdbc961311e1b51f
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2023-12-18 15:05:07 -0600

    installed and initialized swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('APIs')
+    .setDescription('Lists all of our apis.');
+    
+
+
+  const controllers = app.getHttpServer().getModules().map((module) => module.controllers);
+  const controllerNames = controllers.reduce((acc, curr) => acc.concat(curr.map((controller) => controller.name)), []);
+  
+  controllerNames.forEach((controllerName) => {
+    config.addTag(controllerName);
+  });
+
+
+  const document = SwaggerModule.createDocument(app, config.build());
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
