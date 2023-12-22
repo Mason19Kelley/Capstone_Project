178e9b6abd202f7926a005821f9f4b00b428a9e3
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2023-12-22 12:35:58 -0600

    added auth to swagger

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request, Get, Body, Res } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiBasicAuth, ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './auth.model';
+import { Response } from 'express';
 
 @Controller('auth')
+@ApiTags('auth')
+@ApiBearerAuth()
 export class AuthController {
     constructor(private authService: AuthService) {}
 
     @UseGuards(LocalAuthGuard)
+    @ApiBody({ type: LoginDto })
     @Post('login')
-    async login(@Request() req) {
-        console.log(req.query)
-        return this.authService.login(req.query);
+    async login(@Res({ passthrough: true }) response: Response, @Body() credentials: LoginDto) {
+        console.log(credentials)
+        
+        const token = (await this.authService.login(credentials)).access_token;
+        console.log(token)
+        response.cookie('jwt', token);
+        return token
     }
     // testing that auth works
     @UseGuards(JwtAuthGuard)
