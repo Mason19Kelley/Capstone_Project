d8ff084c704a47fcb5a9d43d4c6fdaf58d310971
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-08 21:40:27 -0600

    added get user endpoint

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { OrganizationsModule } from '../organizations/organizations.module';
 import { RolesModule } from '../roles/roles.module';
+import { UsersController } from './users.controller';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), OrganizationsModule, RolesModule],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
