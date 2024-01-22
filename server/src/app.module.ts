71a3e7111ed726d685a2ac5c49b853cb5d29e6af
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-22 14:40:57 -0600

    added docker stuff

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -18,7 +18,7 @@ import { SeedService } from './seed/seed.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgre',
       port: 5432,
       username: "surge-user",
       password: 'password',
