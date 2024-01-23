3622dfa9a8d73632987f3ad52873e315a2891ba6
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-23 12:40:51 -0600

    edited cors config

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
