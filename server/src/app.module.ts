15e0d7d84c22e0fb1885c18c26090723d5d321b2
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-23 16:16:13 -0600

    added db env url config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,31 +12,39 @@ import { Organization } from './organizations/organization.entity';
 import { User } from './users/user.entity';
 import { Role } from './roles/role.entity';
 import { SeedService } from './seed/seed.service';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 // sets up db/typeorm connection and loads all modules into app
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: "surge-user",
-      password: 'password',
-      database: 'surge',
-      entities: [User, Organization, Role],
-      synchronize: true,
-      migrations: ["src/migration/**/*.ts"],
-      
-    }),
-    AuthModule,
-    UsersModule,
-    OrganizationsModule,
-    RolesModule,
-    
+     TypeOrmModule.forRootAsync({
+       imports: [ConfigModule],
+       inject: [ConfigService],
+       useFactory: (configService: ConfigService) => ({
+         type: 'postgres',
+         host: configService.get<string>('ENVIRONMENT') === 'docker' ? 'postgres' : 'localhost',
+         port: 5432,
+         username: "surge-user",
+         password: 'password',
+         database: 'surge',
+         entities: [User, Organization, Role],
+         synchronize: true,
+         migrations: ["src/migration/**/*.ts"],
+       }),
+     }),
+     ConfigModule.forRoot({
+       ignoreEnvFile: true,
+       isGlobal: true
+     }),
+     AuthModule,
+     UsersModule,
+     OrganizationsModule,
+     RolesModule,
   ],
   controllers: [AppController],
   providers: [AppService, SeedService],
-})
+ })
 export class AppModule implements OnApplicationBootstrap  {
   constructor(private readonly seedService: SeedService) {}
   // lifecycle hook on app start
