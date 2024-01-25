a3837f58494266c37a00f06480bf058c74270cb3
FoldsAbigail <afo022@latech.edu>
2024-01-25 00:11:15 -0600

    Fixed Crashing Issue

--- server/src/courses/courses.module.ts
+++ server/src/courses/courses.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Courses } from './courses.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Courses])],
   providers: [CoursesService],
-  controllers: [CoursesController]
+  exports: [CoursesService],
 })
 export class CoursesModule {}
+
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -5,12 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { OrganizationsModule } from '../organizations/organizations.module';
 import { RolesModule } from '../roles/roles.module';
+import { CoursesModule } from '../courses/courses.module'
 import { UsersController } from './users.controller';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), OrganizationsModule, RolesModule],
+  imports: [TypeOrmModule.forFeature([User]), OrganizationsModule, RolesModule, CoursesModule],
   providers: [UsersService, JwtAuthGuard],
   exports: [UsersService],
   controllers: [UsersController],
