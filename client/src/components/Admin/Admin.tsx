59355c2335029bc147a2b10b9d92e61b7b5b8e01
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-26 14:55:25 -0600

    added org management ui

--- client/src/components/Admin/Admin.tsx
+++ client/src/components/Admin/Admin.tsx
@@ -1,11 +1,27 @@
 
+import { Button, Card, Input, Space } from 'antd';
 import './Admin.css'
+import { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
 function Admin() {
+  const { user } = useContext(AuthContext)
   
   return (
-   <div>
-    abc
+   <div className="wrapper">
+      <h1 className="header">Organzation Administration</h1>
+      <Card title="Organization Settings" className='org-management'>
+        <div>
+        <p className="org-name">Organization Name</p>
+        <Space.Compact>
+          <Input defaultValue="" style={{ width: "30vw" }}/>
+          <Button style={{ width: "8vw" }}>Save</Button>
+        </Space.Compact>
+        </div>
+      </Card>
+      <Card style={{ width: '98%' }} className='user-management'>
+        <h2 className='card-title'>User Management</h2>
+      </Card>
    </div>
     
   )
