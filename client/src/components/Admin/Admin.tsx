3e7ebe5f34df4b19fda30b473fd3ed426bf70c99
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-27 22:56:03 -0600

    added basic table, needs css

--- client/src/components/Admin/Admin.tsx
+++ client/src/components/Admin/Admin.tsx
@@ -1,11 +1,51 @@
 
-import { Button, Card, Input, Space } from 'antd';
+import { Button, Card, Input, Space, Table, TableProps } from 'antd';
 import './Admin.css'
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 
+interface DataType {
+  id: number;
+  name: string;
+  role: string;
+  email: string;
+}
+
+const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    key: 'role',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Actions',
+    key: 'action',
+    render: () => <p>...</p>
+  }
+]
+
 function Admin() {
-  const { user } = useContext(AuthContext)
+  // const { user } = useContext(AuthContext)
+
+  let dataSource = [
+    {
+      id: 1,
+      name: "Me",
+      role: "BOSS",
+      email: "ABC@GMAIL.com"
+    }
+  ]
   
   return (
    <div className="wrapper">
@@ -19,8 +59,8 @@ function Admin() {
         </Space.Compact>
         </div>
       </Card>
-      <Card style={{ width: '98%' }} className='user-management'>
-        <h2 className='card-title'>User Management</h2>
+      <Card title="User Management" className='org-management'>
+        <Table columns={columns} dataSource={dataSource}/>
       </Card>
    </div>
     
