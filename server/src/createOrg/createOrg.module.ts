f4c41d6e5e74f78cb14b34a6a8e0c2921ce0d3d8
Jacob <johnboyjacob1@gmail.com>
2024-01-28 13:00:54 -0600

    merging sprint2 into orgCreateAPI to resolve conflicts

--- server/src/createOrg/createOrg.module.ts
+++ server/src/createOrg/createOrg.module.ts
@@ -1,11 +1,8 @@
 import { Module } from "@nestjs/common";
 import { createOrgService } from "./createOrg.service";
 import { createOrgController } from "./createOrg.controller";
-//import { OrganizationsService } from "../organizations/organizations.service";
-//import { UsersService } from "../users/users.service";
 import { OrganizationsModule } from "../organizations/organizations.module";
 import { UsersModule } from "../users/users.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
 
 
 @Module({
