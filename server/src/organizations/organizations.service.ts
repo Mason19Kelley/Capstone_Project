c86dac72d07c383e9c572a38ad0a45f02038021d
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-27 23:52:01 -0600

    rename org endpoint works

--- server/src/organizations/organizations.service.ts
+++ server/src/organizations/organizations.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Organization } from './organization.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { OrgRenameDto } from './orgRename.model';
 // business logic for orgs
 @Injectable()
 export class OrganizationsService {
@@ -12,6 +13,22 @@ export class OrganizationsService {
     // gets org by id
     findOrg(id: number) {
         return this.orgsRepository.findOneBy({id: id })
+    }
+
+    async renameOrg(orgRename: OrgRenameDto) {
+      let id: number = orgRename.id
+      const org = await this.orgsRepository.findOneBy( { id } );
+
+      if (!org) {
+          throw new Error(`Organization with ID ${orgRename.id} not found.`);
+      }
+
+      org.orgName = orgRename.newName;
+
+      await this.orgsRepository.save(org);
+
+      return true
+
     }
 
     // inserts default organization into seed
