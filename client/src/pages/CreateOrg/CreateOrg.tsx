5c72a75fe7b5c61a5bdba73166a641e5ed633ae9
Mason19Kelley <71235579+Mason19Kelley@users.noreply.github.com>
2024-01-28 19:30:01 -0600

    fixed org page

--- client/src/pages/CreateOrg/CreateOrg.tsx
+++ client/src/pages/CreateOrg/CreateOrg.tsx
@@ -9,28 +9,12 @@ const CreateOrg: React.FC = () => {
   const [password, setPassword] = useState('');
   const [organization, setOrgname] = useState('');
   const [adminname, setAdminname] = useState('');
-  const [adminemail, setAdminemail] = useState('');
-  const [adminpassword, setAdminpassword] = useState('');
-  const { isLoggedIn, setLoggedIn, setUser } = useContext(AuthContext)
+  const [email, setEmail] = useState('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/home');
-    }
-  }, [isLoggedIn]);
 
   useEffect(() => {
-    const token = Cookies.get("token");
-    api.defaults.headers['Authorization'] = `Bearer ${token}`;
-    AuthAPI.checkUser().then(response => {
-      console.log(response)
-      setUser(response)
-      setLoggedIn(true)
-      navigate("/home")
-    }).catch(error => 
-      console.log(error)
-    )
+    
   }, [])
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -45,7 +29,7 @@ const CreateOrg: React.FC = () => {
 
   return (
     <section>
-      <div className="form-box">
+      <div className="org-box">
         <div className="form-value">
           <form onSubmit={handleSubmit}>
             <h2>Create an Organization</h2>
@@ -54,15 +38,19 @@ const CreateOrg: React.FC = () => {
               <label>Organization Name</label>
             </div>
             <div className="inputbox">
-              <input type="adminname" required value={password} onChange={(e) => setPassword(e.target.value)} />
+              <input type="adminname" required value={adminname} onChange={(e) => setAdminname(e.target.value)} />
               <label>Administrator Name</label>
             </div>
             <div className="inputbox">
-              <input type="adminemail" required value={adminemail} onChange={(e) => setAdminemail(e.target.value)} />
+              <input type="username" required value={username} onChange={(e) => setUsername(e.target.value)} />
+              <label>Administrator Username</label>
+            </div>
+            <div className="inputbox">
+              <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
               <label>Administrator Email</label>
             </div>
             <div className="inputbox">
-              <input type="adminpassword" required value={adminpassword} onChange={(e) => setAdminpassword(e.target.value)} />
+              <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
               <label>Administrator Password</label>
             </div>
             <button type="submit" >Create</button>
